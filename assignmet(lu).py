d513f808dcd4dc6e93a365186aa99f5b45d6c263
MOHAN KRISHNA MOLAKALA <126973904+MKTHOR22@users.noreply.github.com>
2023-09-02 11:18:36 +0530

    Add files via upload

---
+++ assignmet(lu).py
@@ -0,0 +1,15 @@
+# Taking two inputs from the user
+a = float(input("Enter the first number (a): "))
+b = float(input("Enter the second number (b): "))
+
+# 1. Floor Division
+floor_division_result = a // b
+print("Floor Division (a // b):", floor_division_result)
+
+# 2. a power b
+power_result = a ** b
+print("a to the power of b (a ** b):", power_result)
+
+# 3. Check if two variables have equal value
+are_equal = a == b
+print("Are a and b equal?", are_equal)
